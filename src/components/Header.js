944a9df7d8bece5493959b5f050bbd5bd232fc4b
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2019-09-25 22:47:35 -0700

    init sitter swap react

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="sitter-swap-header">
+        <h1>SitterSwap</h1>
+        <h2>Connecting Families With Trusted Child Care</h2>
+      </div>
+    )
+  }
+}
+
+export default Header;
