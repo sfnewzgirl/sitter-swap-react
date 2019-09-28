68a483edc67beec490a05b9dcdb7654faefa644c
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2019-09-28 13:59:40 -0700

    init css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
+import HeaderMenu from './HeaderMenu';
 import React from 'react';
+import './Header.css';
 
 class Header extends React.Component {
   render() {
     return (
       <div className="sitter-swap-header">
-        <h1>SitterSwap</h1>
-        <h2>Connecting Families With Trusted Child Care</h2>
+        <div className="sitter-swap-logo">
+          <h1>SitterSwap</h1>
+          <p>Connecting Families With Trusted Child Care</p>
+        </div>
+        <div className="sitter-swap-header-menu">
+          <HeaderMenu/>
+        </div>
       </div>
     )
   }
---
+++ src/components/HeaderMenu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HeaderMenu.css';
+
+class HeaderMenu extends React.Component {
+  render() {
+    return (
+      <div className="header-menu-buttons">
+        <p>One</p>
+        <p>Two</p>
+        <p>Three</p>
+      </div>
+    )
+  }
+}
+
+export default HeaderMenu;
